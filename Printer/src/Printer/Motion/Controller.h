6655237b21be44f79f82f07ae04c2f7d9a998116
Onur Tuncer <onur.tuncer@melina-aero.com>
2024-01-29 11:59:27 +0300

    Update Controller.h

--- Printer/src/Printer/Motion/Controller.h
+++ Printer/src/Printer/Motion/Controller.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Interpreter.h"
+#include "Stepper.h"
 
 namespace Printer::Motion{
 
@@ -14,10 +15,11 @@ namespace Printer::Motion{
       PausePrinting();
 
     private:
+      typedef std::string::const_iterator Iterator;
+      RS274NGCGrammar<Iterator> m_Parser;
 
       bool m_IsPaused{false};
       
-   
 
 
   }
